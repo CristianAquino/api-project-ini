878c0cdf51863c23580a2b96081453f0114068bc
cristianaquino <ycristian10@gmail.com>
2025-04-17 22:09:36 -0500

    feat: add config jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUuids;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +49,31 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // implementacion de metodos para JWTAuth
+    public function getJWTIdentifier()
+    {
+        return $this->getKey(); // ID del usuario
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    // Mutator para encriptar automáticamente la contraseña
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    // relations
+    function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+    function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
