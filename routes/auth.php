1aa70b8a97ae7fba16adf4fb224aeb3488f2322c
cristianaquino <ycristian10@gmail.com>
2025-04-20 20:42:57 -0500

    feat: add middleware

--- routes/auth.php
+++ routes/auth.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
+Route::controller(AuthController::class)
+    ->group(function () {
+        Route::post('/login', 'login');
+        Route::post('/register', 'register');
+    });
 
-    Route::post('/login', 'login');
-    Route::post('/register', 'register');
-    Route::post('/logout', 'logout');
-});
+Route::controller(AuthController::class)
+    ->middleware([
+        'auth_cookie',
+        'jwt_valid'
+    ])
+    ->group(function () {
+        Route::post('/logout', 'logout');
+    });
