8c38809d0343caf4ca2eefd7c25564da1d048312
cristianaquino <ycristian10@gmail.com>
2025-04-20 20:42:09 -0500

    feat: add middleware for route user

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(base_path('routes/auth.php'));
+Route::prefix('user')
+    ->middleware([
+        'auth_cookie',
+        'jwt_valid'
+    ])
+    ->group(base_path('routes/user.php'));
 Route::prefix('article')->group(base_path('routes/article.php'));
 Route::prefix('comment')->group(base_path('routes/comment.php'));
