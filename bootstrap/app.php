7db9dd879d7d1b003822f03a64a0ab9bbf4106c9
cristianaquino <ycristian10@gmail.com>
2025-04-20 20:41:18 -0500

    feat: add aliases for middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AuthenticateWithCookieMiddleware;
+use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'auth_cookie' => AuthenticateWithCookieMiddleware::class,
+            'jwt_valid' => JWTMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
