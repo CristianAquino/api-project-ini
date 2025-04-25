3cce5e39d31c807cab4f3e398debd217c5dc2b74
cristianaquino <ycristian10@gmail.com>
2025-04-24 23:14:38 -0500

    feat: controller and route for user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DTOs\UserDTO;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $users = User::query()
+            ->paginate(10);
+        $usersDTO = UserDTO::fromPagination($users);
+        return response()->json($usersDTO, Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function me()
+    {
+        $user = Auth::user();
+        $userDTO = UserDTO::fromBaseModel($user);
+        return response()->json($userDTO, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        //
+        $me = Auth::id();
+        User::where('id', $me)->delete();
+        return response()->json([
+            'message' => 'User deleted successfully'
+        ], Response::HTTP_ACCEPTED);
+    }
+}
