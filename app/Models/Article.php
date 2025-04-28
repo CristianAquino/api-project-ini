27f03fc474fede7d54bc1d25575cbf4f4417756b
cristianaquino <ycristian10@gmail.com>
2025-04-27 19:39:03 -0500

    feat: add default user auth

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Article extends Model
 {
@@ -16,9 +17,8 @@ class Article extends Model
     protected $fillable = [
         'title',
         'content',
-        'user_id',
+        'category_id',
     ];
-
 
     // relations
     function user(): BelongsTo
@@ -32,5 +32,19 @@ class Article extends Model
     function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    // este metodo sirve para que cada vez
+    // que creamos un comentario, el id del
+    // usuario identificado sea setiado
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($article) {
+            if (Auth::check()) {
+                $article->user_id = Auth::id();
+            }
+        });
     }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Comment extends Model
 {
@@ -13,9 +14,8 @@ class Comment extends Model
 
     protected $fillable = [
         'comment',
-        'user_id',
-        'article_id',
     ];
+
 
     // relations
     function user(): BelongsTo
@@ -25,5 +25,19 @@ class Comment extends Model
     function article(): BelongsTo
     {
         return $this->belongsTo(Article::class);
+    }
+
+    // este metodo sirve para que cada vez
+    // que creamos un comentario, el id del
+    // usuario identificado sea setiado
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($comment) {
+            if (Auth::check()) {
+                $comment->user_id = Auth::id();
+            }
+        });
     }
 }
