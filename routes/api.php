9d4619852f1abcec5e67d794fe1c9ad1fbd4b3f2
cristianaquino <ycristian10@gmail.com>
2025-04-30 19:31:40 -0500

    feat: routes

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(base_path('routes/auth.php'));
+// auth routes
+Route::prefix('auth')
+    ->middleware([
+        'auth_cookie',
+        'jwt_valid'
+    ])->group(base_path('routes/auth.php'));
+// user routes
 Route::prefix('user')
     ->middleware([
         'auth_cookie',
         'jwt_valid'
     ])
     ->group(base_path('routes/user.php'));
-Route::prefix('articles')->group(base_path('routes/article.php'));
-Route::prefix('comments')->group(base_path('routes/comment.php'));
+// articles routes
+Route::prefix('articles')
+    ->middleware([
+        'auth_cookie',
+        'jwt_valid'
+    ])->group(base_path('routes/article.php'));
+// comments routes
+Route::prefix('comments')
+    ->middleware([
+        'auth_cookie',
+        'jwt_valid'
+    ])->group(base_path('routes/comment.php'));
--- routes/auth.php
+++ routes/auth.php
@@ -3,17 +3,12 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)
-    ->group(function () {
-        Route::post('/login', 'login');
-        Route::post('/register', 'register');
-    });
-
-Route::controller(AuthController::class)
-    ->middleware([
-        'auth_cookie',
-        'jwt_valid'
-    ])
-    ->group(function () {
-        Route::post('/logout', 'logout');
-    });
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/logout', 'logout')
+        ->withoutMiddleware([
+            'auth_cookie',
+            'jwt_valid'
+        ]);
+});
